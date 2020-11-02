b7e3401816c8207bde7d852f1bcdd5d0882f0538
Swami Venkatesan <swami.venkatesan@mindmics.com>
2020-11-01 21:31:57 -0500

    Changed the eda maps and added a correlation notebook

--- src/utils/collect_data.py
+++ src/utils/collect_data.py
@@ -48,14 +48,20 @@ class DataCollection():
 
         df_mobility['weekend'] = df_mobility['weekday'].apply(lambda x: self.weekend(x))
         df_mobility['month'] = df_mobility.date.dt.month_name()
-        df_mobility['Wk #'] = (df_mobility.date.dt.week.astype(str) ) #+ ' ' + (df_mobility.date.dt.year.astype(str) )
+        df_mobility['Wk #'] = (df_mobility.date.dt.week.astype(str) )
 
         trip_col = [col for col in df_mobility.columns if 'Number of' in col]
+
         avg_mil = np.array([0.5, 2, 4, 7.5, 17.5, 37.5, 75, 175, 375, 750])
+        abg_mil_short = np.array([0.5, 2, 4, 7.5, 17.5, 37.5, 0, 0, 0, 0])
         df_mobility['mobility_per_trip'] = np.sum(df_mobility[trip_col[1:]].multiply(avg_mil, axis=1), axis=1) / df_mobility[
             trip_col[0]]
         df_mobility['mobility_per_person'] = np.sum(df_mobility[trip_col[1:]].multiply(avg_mil, axis=1), axis=1) / (df_mobility['Population Staying at Home'] + df_mobility[
                                            'Population Not Staying at Home'])
+        df_mobility['mobility_per_trip_short'] = np.sum(df_mobility[trip_col[1:]].multiply(abg_mil_short, axis=1), axis=1) / \
+                                           df_mobility[trip_col[0]]
+        df_mobility['mobility_per_person_short'] = np.sum(df_mobility[trip_col[1:]].multiply(abg_mil_short, axis=1), axis=1) / (
+                    df_mobility['Population Staying at Home'] + df_mobility['Population Not Staying at Home'])
         df_mobility['pct_stay_home'] = df_mobility['Population Staying at Home'] * 100 / (df_mobility['Population Staying at Home'] + df_mobility[
                                            'Population Not Staying at Home'])
         df_mobility['trips_per_person'] = df_mobility['Number of Trips'] / (df_mobility['Population Not Staying at Home'] + df_mobility['Population Staying at Home'] )
@@ -334,7 +340,7 @@ class DataCollection():
 
     def combine_weather(self,df_mobility,df_covid):
         start_date = datetime.datetime.strftime(df_mobility.date.max(), '%Y-%m-%d')
-        fpath = './Data/All_County_Weather_' + start_date + '.csv'
+        fpath = './Data/datasets/Weather/All_County_Weather_' + start_date + '.csv'
         if os.path.exists(fpath):
             df_weather = pd.read_csv(fpath, parse_dates=['date'])
         else:
--- src/utils/eda.py
+++ src/utils/eda.py
@@ -1,10 +1,8 @@
 import folium
-import seaborn
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
 import geopandas as gpd
-from src.utils import collect_data
 from branca.element import MacroElement
 from jinja2 import Template
 import numpy as np
@@ -14,6 +12,8 @@ from branca.colormap import LinearColormap
 import datetime
 import os
 from random import randint
+from folium.plugins import TimeSliderChoropleth
+
 
 # Create a class to bind color maps to respective geojson maps
 
@@ -83,28 +83,51 @@ class DataAnalysis():
                             df_gr.mean(axis=1).mean() + 5 * df_gr.mean(axis=1).std())
 
     # Creating functions to create map/json and color scales
-    def generate_geojson_map(self,df, col_name, tooltip_col=None, name=None):
+    def get_county_state(self,val_dict, x):
+        return val_dict[x]
+    def get_df_loc(self,df):
+        df_county_geocodes = pd.read_csv('./Data/County_GEOCODES-v2017.csv', encoding='latin')
+        df_county_geocodes['fips'] = df_county_geocodes['fips'].astype(str).str.zfill(5)
+        county_dict = df_county_geocodes.set_index('fips')['long_name'].to_dict()
+        df['loc'] = df['fips'].apply(lambda x: self.get_county_state(county_dict, x))
+        return df
+    def generate_geojson_map(self,df, col_name,cmap,q_filter,filter_level, tooltip_col=None, name=None):
+        if 'loc' not in df.columns:
+            df = self.get_df_loc(df)
         df = df.rename(columns={'fips': 'GEO_ID'})
+        if q_filter:
+            if filter_level:
+                low_fil = filter_level[0]
+                high_fil = filter_level[1]
+                df.loc[df[col_name] > df[col_name].quantile(high_fil), col_name] = df[col_name].quantile(high_fil)
+                df.loc[df[col_name] < df[col_name].quantile(low_fil), col_name] = df[col_name].quantile(low_fil)
+            else:
+                print('No outlier filtering done. Please filter levels as a list')
+
         mdict = df.set_index('GEO_ID')[col_name].to_dict()
 
         fpath = self.create_geo_json(df, col_name)
         with open(fpath, encoding="ISO-8859-1") as json_file:
             county_geo = json.load(json_file)
         gmap = folium.GeoJson(
-            data=county_geo, name=name,
+            data=county_geo, name=name,overlay=True,
             tooltip=folium.GeoJsonTooltip(fields=[tooltip_col, col_name]),
             style_function=lambda feature: {
-                'fillColor': self.get_color(mdict, feature),
+                'fillColor': self.get_color(mdict, feature,cmap),
                 'fillOpacity': 0.7,
                 'color': 'black',
                 'weight': 0.5,
             }
         )
-        color = plt.get_cmap("plasma")
-
-        min_color = rgb2hex(color.colors[0][:3])
-        mid_color = rgb2hex(color.colors[127][:3])
-        max_color = rgb2hex(color.colors[-1][:3])
+        color = plt.get_cmap(cmap)
+        if isinstance(color, matplotlib.colors.LinearSegmentedColormap):
+            min_color = rgb2hex(color(0)[:3])
+            mid_color = rgb2hex(color(127)[:3])
+            max_color = rgb2hex(color(256)[:3])
+        else:
+            min_color = rgb2hex(color.colors[0][:3])
+            mid_color = rgb2hex(color.colors[127][:3])
+            max_color = rgb2hex(color.colors[-1][:3])
         if isinstance(min(mdict.values()),datetime.datetime):
             min_val = 0
             max_val = (max(mdict.values()) - min(mdict.values())).days
@@ -143,8 +166,8 @@ class DataAnalysis():
 
         return (fpath)
 
-    def get_color(self,map_dict, feature):
-        color = plt.get_cmap("plasma")
+    def get_color(self,map_dict, feature,cmap):
+        color = plt.get_cmap(cmap)
         min_val = min(map_dict.values())
         max_val = max(map_dict.values())
         if isinstance(min_val, datetime.datetime):
@@ -153,14 +176,18 @@ class DataAnalysis():
         else:
             color_sc = 255 / (abs(min_val) + abs(max_val))
             value = int((map_dict.get(feature['properties']['GEO_ID']) + abs(min_val)) * color_sc)
-        rgb = color.colors[value][:3]
+        if isinstance(color, matplotlib.colors.LinearSegmentedColormap):
+            rgb = color(value)[:3]
+        else:
+            rgb = color.colors[value][:3]
         return str((matplotlib.colors.rgb2hex(rgb)))
-    def plot_map(self, col_list,df,save_op = False):
+    def plot_map(self, col_list,df,save_op = False,cmap='plasma',q_filter=False,filter_level=[0.01,0.99]):
         us_cen = [43.8283, -98.5795]
         base_map = folium.Map(location=us_cen, zoom_start=4)
         maps, cs = [], []
         for col in col_list:
-            gmap, color_scale = self.generate_geojson_map(df, col, name=col, tooltip_col='loc')
+            gmap, color_scale = self.generate_geojson_map(df, col, name=col, tooltip_col='loc',cmap=cmap,
+                                                          q_filter=q_filter,filter_level=filter_level)
             maps.append(gmap)
             cs.append(color_scale)
         for m in maps:
@@ -179,3 +206,85 @@ class DataAnalysis():
             print('Saving file at ', fpath)
             base_map.save(fpath)
         return base_map
+
+
+    def get_style_dict(self,df, df_geo):
+        def get_state_id(x, sdict):
+            if x in sdict.keys():
+                return sdict[x]
+            else:
+                return None
+
+        sdict = df_geo.reset_index().set_index('GEO_ID')['index'].to_dict()
+
+        df['id'] = (df['fips'].apply(lambda x: get_state_id(x, sdict)))
+        df = df.dropna()
+        df['id'] = df['id'].astype(int)
+        styledata = {}
+        for st in df.id.unique():
+            df_st = df[df.id == st][['date', 'color', 'opacity']].sort_values('date').set_index('date')
+            styledata[st] = df_st
+        styledict = {
+            str(country): data.to_dict(orient='index') for
+            country, data in styledata.items()}
+        return styledict
+
+    def generate_ts_geo(self,df_geo,styledict, df, targ_var, name):
+
+        g = TimeSliderChoropleth(
+            df_geo.to_json(),
+            styledict=styledict, name=name)
+        color = plt.get_cmap("plasma")
+        min_color = rgb2hex(color.colors[0][:3])
+        mid_color = rgb2hex(color.colors[127][:3])
+        max_color = rgb2hex(color.colors[-1][:3])
+
+        min_val = (df[targ_var].min())
+        max_val = (df[targ_var].max())
+        color_scale = LinearColormap([min_color, mid_color, max_color],
+                                     vmin=min_val,
+                                     vmax=max_val)
+        return g, color_scale
+
+    def get_rgb_col(self,x):
+        import matplotlib
+        color = plt.get_cmap("plasma")
+        rgb = color.colors[x][:3]
+        return str(matplotlib.colors.rgb2hex(rgb))
+
+    def group_df(self,df,targ_var,q_filter):
+        df_gr = df.groupby(['fips', 'month'])[targ_var].mean().reset_index()
+        df_gr = df_gr.fillna(df_gr.mean())
+        if q_filter:
+            df_gr.loc[df_gr[targ_var] > df_gr[targ_var].quantile(0.99), targ_var] = df_gr[targ_var].quantile(0.99)
+
+        min_val = (df_gr[targ_var].min())
+        max_val = (df_gr[targ_var].max())
+        col_scl = 255 / (abs(min_val) + abs(max_val))
+        df_gr['color'] = (((df_gr[targ_var]) - abs(min_val)) * col_scl).astype(int)
+        df_gr['color'] = df_gr['color'].apply(lambda x: self.get_rgb_col(x))
+        df_gr['date'] = df_gr["month"].apply(lambda x: datetime.datetime.strptime('2019-' + str(x) + '-02', "%Y-%B-%d"))
+        df_gr.date = ((df_gr['date'].astype('int64')) // 1e9).astype('U10')
+
+        df_gr['opacity'] = 0.75
+        return df_gr
+    def plot_ts_map(self,df,targ_var,q_filter):
+        df_geo = gpd.read_file('./Data/geometry/county.geojson', driver='GeoJSON')
+
+        df_geo['GEO_ID'] = df_geo['GEO_ID'].astype(str).str[-5:]
+        df_gr = self.group_df(df,targ_var,q_filter)
+        styledict = self.get_style_dict(df_gr, df_geo)
+
+        g, color_scale = self.generate_ts_geo( df_geo,styledict, df_gr,targ_var,  name=targ_var)
+        us_cen = [43.8283, -98.5795]
+
+        base_map = folium.Map(location=us_cen, zoom_start=4, tiles='cartodbpositron')
+        base_map.add_child(g)
+        base_map.add_child(folium.map.LayerControl())
+
+        base_map.add_child(color_scale)
+        base_map.add_child(BindColormap(g, color_scale))
+
+
+        return base_map
+
